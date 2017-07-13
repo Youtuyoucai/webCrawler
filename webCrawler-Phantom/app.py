b81fc8844ad72290d7e2b250edc4de84d6b6a0ba
Stanley Jia <stanleyjia101@gmail.com>
2017-07-13 09:46:04 -0700

    More info

--- webCrawler-Phantom/app.py
+++ webCrawler-Phantom/app.py
@@ -1,6 +1,6 @@
 #!/bin/sh
 # coding: utf-8
-
+#https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyBNphpzmEaN30wVqQnOKYf0ATlXcnyDzX0&location=37.7823283,-122.2893865&radius=200
 # In[1]:
 from flask import Flask, render_template, request, jsonify, abort, make_response, Response, redirect
 from selenium import webdriver
@@ -19,12 +19,64 @@ import chardet
 from aip import AipFace
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import string
+import requests
+import json
 
 app = Flask(__name__)
 
 caps = DesiredCapabilities.PHANTOMJS
 caps["phantomjs.page.settings.userAgent"] = "Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1"
+
+
+varAndXPath = {
+                "estimate": "//*[@id='content']/div[5]/div[2]/div/div/div/div[2]/div[1]/div/div[1]/div",
+                'beds':"//*[@id='basicInfo']/div[2]/div[1]/div[1]/div",
+                'baths':"//*[@id='basicInfo']/div[2]/div[1]/div[2]/div",
+                'finishedSqFt':"//*[@id='basicInfo']/div[2]/div[1]/div[3]/div",
+                'unfinishedSqFt':"//*[@id='basicInfo']/div[2]/div[1]/div[4]/div",
+                'totalSqFt':"//*[@id='basicInfo']/div[2]/div[1]/div[5]/div",
+                'floors':"//*[@id='basicInfo']/div[2]/div[1]/div[6]/div",
+                'lotSize':"//*[@id='basicInfo']/div[2]/div[1]/div[7]/div",
+                'style':"//*[@id='basicInfo']/div[2]/div[1]/div[7]/div",
+                'yearRenovated':"//*[@id='basicInfo']/div[2]/div[1]/div[10]/div",
+                'county':"//*[@id='basicInfo']/div[2]/div[1]/div[11]/div",
+                'apn':"//*[@id='basicInfo']/div[2]/div[1]/div[12]/div",
+                "units":"//*[@id='property-details-scroll']/div/div[2]/div/div/div[1]/div[2]/div[1]/ul/div/li/span",
+                "rooms":"//*[@id='property-details-scroll']/div/div[2]/div/div/div[1]/div[2]/div[2]/ul/div/li/span",
+                "primaryGarageType":"//*[@id='property-details-scroll']/div/div[2]/div/div/div[2]/div[2]/div[1]/ul/div/li[1]/span",
+                "parkingType":"//*[@id='property-details-scroll']/div/div[2]/div/div/div[2]/div[2]/div[1]/ul/div/li[2]/span",
+                "structuralStyle":"//*[@id='property-details-scroll']/div/div[2]/div/div/div[2]/div[2]/div[2]/ul/div/li[1]/span",
+                #"frameworkStructuralMaterial": "//*[@id='property-details-scroll']/div/div[2]/div/div/div[2]/div[2]/div[2]/ul/div/li[2]/span",
+                #"constructionQuality":"//*[@id='property-details-scroll']/div/div[2]/div/div/div[2]/div[2]/div[2]/ul/li[1]/span",
+                #"buildingShape":"//*[@id='property-details-scroll']/div/div[2]/div/div/div[2]/div[2]/div[2]/ul/li[2]/span",
+                #"lastLandAppraisal":"//*[@id='property-details-scroll']/div/div[2]/div/div/div[2]/div[2]/div[3]/ul/div/li/span",
+                #"lotSize":"//*[@id='property-details-scroll']/div/div[2]/div/div/div[3]/div[2]/div[1]/ul/div/li/span",
+                "buildingSqFt":"//*[@id='property-details-scroll']/div/div[2]/div/div/div[3]/div[2]/div[2]/ul/div/li[1]/span",
+                #"totalStories":"//*[@id='property-details-scroll']/div/div[2]/div/div/div[3]/div[2]/div[2]/ul/div/li[2]/span",
+                #"totalStructuralSqFt":"//*[@id='property-details-scroll']/div/div[2]/div/div/div[3]/div[2]/div[2]/ul/div/li[2]/span",
+                "totalStructureDesc":"//*[@id='property-details-scroll']/div/div[2]/div/div/div[3]/div[2]/div[2]/ul/li[2]/span",
+                #"allBuildingsOnPropertySqFt": "//*[@id='property-details-scroll']/div/div[2]/div/div/div[3]/div[2]/div[2]/ul/li[3]/span",
+                #"totalAssessorSqFt": "//*[@id='property-details-scroll']/div/div[2]/div/div/div[3]/div[2]/div[2]/ul/li[4]/span",
+                #"permittedRemodelYear": "//*[@id='property-details-scroll']/div/div[2]/div/div/div[3]/div[2]/div[2]/ul/li[5]/span",
+               # "numOfStructures": "//*[@id='property-details-scroll']/div/div[2]/div/div/div[3]/div[2]/div[2]/ul/li[6]/span"
+            }
+
+zillowXPaths = {
+    "estimate": "//*[@id='home-value-wrapper']/div[1]/div[2]/span[2]",
+    "beds": "//*[@id='hdp-content']/div[2]/div[1]/div[1]/header/h3/div/span[2]",
+    "baths": "//*[@id='hdp-content']/div[2]/div[1]/div[1]/header/h3/div/span[4]",
+    "sqft": "//*[@id='hdp-content']/div[2]/div[1]/div[1]/header/h3/div/span[6]",
+    "yearBuilt": "//*[@id='hdp-content']/div[2]/div[1]/section[1]/div[2]/div/div[1]/div[2]/div/div[2]/div",
+    "lotSize": "//*[@id='hdp-content']/div[2]/div[1]/section[1]/div[2]/div/div[1]/div[6]/div/div[2]/div",
+    "roomCount": "//*[@id='yui_3_18_1_2_1498856895577_951']/div[2]/div[1]/div[3]/ul/li/span[2]"
+}
+
+
 
 
 def is_number(s):
@@ -34,21 +86,102 @@ def is_number(s):
     except ValueError:
         return False
     
+def getExactAddress(searchString):
+    gmapresult = requests.get("http://maps.googleapis.com/maps/api/geocode/json?address="+searchString).text
+    jdata = json.loads(gmapresult)
+    status = jdata.get('status')
+    if (status == "OK"):
+        results = jdata.get('results')
+        result = results[0]
+        geography = result['geometry']
+        location = geography["location"]
+        lat = repr(location["lat"])
+        lng = repr(location["lng"])
+        requrl = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lng+"&key=AIzaSyBNphpzmEaN30wVqQnOKYf0ATlXcnyDzX0"
+        addresult = requests.get(requrl).text
+        jdata = json.loads(addresult)
+        results = jdata.get('results')
+        for item in results:
+            types = item.get('types')
+            if "premise" in types:
+                return item.get('formatted_address')
+                
+    elif (status == "ZERO_RESULTS"):
+        return "Address non-existent in Google Maps"
+    else:
+        return "Google Maps " + status
+        
+            
+
+def getSurroundingValues(searchString):
+    
+    array = searchString.split('+')
+    redfinString="https://www.google.com/#q="+searchString +"redfin"
+    print(redfinString)
+    driver.get(redfinString)
+    source = driver.page_source
+    bsObj = BeautifulSoup(source, "html.parser")
+    #urls for nearby properties
+    sibling = bsObj.findAll("h3", { "class" : "r" })
+    
+    if len(sibling) >= 1:
+        link = sibling[0].a['href']
+        print(link)
+        link
+    else:
+        return 'search not found'
+
+    correct_link = True
+
+    #check if correct link
+    for word in array:
+        if is_number(word) == True:
+            if word in link:
+                correct_link = True
+            else:
+                correct_link = False
+                print word
+                break
+
+    if correct_link == True:
+        print('correct')
+        count = 0
+        driver.get(link)
+        #print('good link')
+        source = driver.page_source
+        bsObj = BeautifulSoup(source, "html.parser")
+        sibling = bsObj.findAll("a", { "class" : "nearby-home-address" })
+        urls = []
+        while count < 1:
+            for item in sibling:
+                urls.append("redfin.com"+item["href"])
+                count += 1
+        for url in urls:
+            driver.get(url)
+    
     
 def search_top(search_string, array, site):
+    def tryPath(xpath):
+            try:
+                item = driver.find_element(By.XPATH, xpath)
+                return item.text
+            except (NoSuchElementException):
+                return 'not found'
+            
+    addressInfo = {}
     driver.get(search_string)
-
+    
 
     # In[2]:
 
     source = driver.page_source
     bsObj = BeautifulSoup(source, "html.parser")
+    
     sibling = bsObj.findAll("h3", { "class" : "r" })
     
     if len(sibling) >= 1:
         link = sibling[0].a['href']
         link
-        print(link)
     else:
         return 'search not found'
 
@@ -65,43 +198,64 @@ def search_top(search_string, array, site):
 
     if correct_link == True:
         driver.get(link)
-
-
+        #print('good link')
         source = driver.page_source
         bsObj = BeautifulSoup(source, "html.parser")
 
         if site == 0:
             #redfin
-            sibling = bsObj.findAll("div", { "class" : "info-block avm" })
-            text = "Price Unknown"
-            if len(sibling) >= 1:
-                text = sibling[0].find("div", { "class" : "statsValue" }).get_text()
-
-
-            print(text)
-            return text
+            try:
+                #print('waiting')
+                element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//*[@id='content']/div[5]/div[2]/div/div/div/div[2]/div[1]/div/div[2]/div")))
+            finally:
+                for var, xpath in varAndXPath.iteritems():
+                    item = tryPath(xpath)
+                    addressInfo[var] = item 
+                
+                
+                return jsonify(addressInfo)
 
         elif site == 1:
-            value = 0
+            estimate = "Price Unknown"
+            rentEstimate = "Price Unknown"
             #zillow
+            
             sibling = bsObj.findAll("div", { "class" : "home-summary-row" })
             for item in sibling:
-                if 'Zestimate' in item.get_text():
+                if '  Zestimate' in item.get_text():
                     if len(item.get_text().split('$')) >= 2:
-                        value = item.get_text().split('$')[1]
-
-            return value
-
-
-            '''text = sibling[0].span.get_text()
-            print text
-            return text'''
+                        estimate = item.get_text().split('$')[1]
+                if 'Rent Zestimate' in item.get_text():
+                    if len(item.get_text().split('$')) >= 2:
+                        rentEstimate = item.get_text().split('$')[1]
             
+            sibling1 = bsObj.findAll("span", { "class" : "addr_bbs" })
+            sibling2 = bsObj.findAll("div", { "class" : "hdp-fact-ataglance-value" })
+            
+            return jsonify({
+                'estimate':estimate,
+                'rentEstimate':rentEstimate,
+                'beds': sibling1[0].get_text(),
+                'baths': sibling1[1].get_text(),
+                'sqft': sibling1[2].get_text(),
+                "yearBuilt": sibling2[1].get_text(),
+                "type": sibling2[0].get_text(),
+                "yearBuilt": sibling2[1].get_text(),
+                "Heating": sibling2[2].get_text(),
+                "Cooling": sibling2[3].get_text(),
+                "Parking": sibling2[4].get_text(),
+                "Lot": sibling2[5].get_text(),
+            })
    
-    return 'not found'
+    else: 
+        #print('bad link')
+        return False
+
+
 driver = webdriver.PhantomJS('./linux/phantomjs', desired_capabilities=caps)  # Optional argument, if not specified will search path.
 
-@app.route('/getprice', methods=['POST'])
+@app.route('/getinfo', methods=['POST'])
 def getPrice():
     address = request.json.get('address').lower()
  
@@ -112,23 +266,27 @@ def getPrice():
         searchString+=word 
         searchString+="+"
         
-    searchString = "https://www.google.com/#q="+searchString
+    exactAddress = getExactAddress(searchString)
+    #print exactAddress
+    getSurroundingValues(searchString)
     
-    redfinString=searchString +"redfin"
-    zillowString=searchString +"zillow"
+    redfinString="https://www.google.com/#q="+searchString +"redfin"
+    zillowString="https://www.google.com/#q="+searchString +"zillow"
     
     
-
-        
     result = search_top(redfinString, array, 0)
-    if result != 'not found':
-        return(jsonify({"estimate":result}))
+    if result != False:
+        return(result)
     else:
         zresult = search_top(zillowString, array, 1)
-        if zresult != 'not found':
-            return(jsonify({"estimate":zresult}))
+        if zresult != False:
+            return(zresult)
         else:
-            return 'Address not found'
+            #value house from nearby houses
+            exactAddress = getExactAddress(searchString)
+            
+            
+           
     
     
 if __name__ == '__main__':
